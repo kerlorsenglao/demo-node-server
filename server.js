7d9d63544c40f42438aaf9738f86d8164780ae35
kerlorsenglao <35926511+kerlorsenglao@users.noreply.github.com>
2019-12-09 17:16:01 +0700

    GET: user by searching with a latter of user name

--- server.js
+++ server.js
@@ -8,6 +8,28 @@ app.set('views','./views');
 app.get('/', function(req,res){
    res.render('index');
 });
+
+var users = [
+		{id: 1, name: 'Kerlor'},
+		{id: 2, name: 'ViengChang'}
+		]
+app.get ('/users',function(req,res){
+	res.render('users/index',{
+		users
+	});
+});
+
+app.get('/users/search',function(req,res){
+	var q= req.query.q;
+	var matchedUsers = users.filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+	res.render('users/index',{
+		users : matchedUsers
+	});
+});
+
+
 app.listen(port, function(){
    console.log('Server listening on port'+port);
 });
